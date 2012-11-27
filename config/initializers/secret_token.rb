100700e44101280897e662b8d24e00e3903368f1
zh99998 <zh99998@my-card.in>
2012-11-27 22:29:34 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MycardUsers::Application.config.secret_token = 'b131914c7347f200e5c4f274d236c94e5281ff2c4b0312e38442f41fd4460d7d8e2d403d58e46548dc46ced57e607e11bf4a33ddb3fd81882426a03322a83b5d'
